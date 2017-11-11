a1ac155f42b16f720ab0456a973f4ecf842df615
Robert Crowdis <RobertCrowdis@gmail.com>
2017-11-11 14:49:42 -0500

    feat: design cards on page

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -8,6 +8,22 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ResourcesComponent implements OnInit {
 
+  dummyResources: any[] = [{
+      'title': 'Dummy Data One',
+      'content': 'Dummy Content',
+      'url': 'Https/dummycontent.url'
+    },
+    {
+      'title:': 'How to make your classroom nicer, right?',
+      'content': 'wompwomp womp WOMP!',
+      'url': 'https/moredummydata'
+    },
+    {
+      'title': 'Title',
+      'content': 'Content',
+      'url': 'url',
+    }];
+
   constructor() { }
 
   ngOnInit() {
