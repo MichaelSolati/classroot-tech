bffb16c4309e3154c09273403a814e04eb76273d
Michael Solati <mkslt04@gmail.com>
2017-11-11 20:44:51 -0500

    feat(result): query result to display

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,14 +1,17 @@
 import { UploaderService } from './uploader.service';
 import { ResourcesService } from './resources.service';
 import { QuestionsService } from './questions.service';
+import { ClassroomsService } from './classrooms.service';
 
 export * from './uploader.service';
 export * from './resources.service';
 export * from './questions.service';
+export * from './classrooms.service';
 
 export const SERVICES: any[] = [
   UploaderService,
   ResourcesService,
-  QuestionsService
+  QuestionsService,
+  ClassroomsService
 ];
 
