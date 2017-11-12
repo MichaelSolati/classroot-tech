eb44990385b3fdcef74013646f2e8202b89cdc2e
Michael Solati <mkslt04@gmail.com>
2017-11-12 06:03:38 -0500

    refactor: fix some styling

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { environment } from '../../environments/environment';
 import { SERVICES } from './services';
@@ -11,8 +10,7 @@ import { SERVICES } from './services';
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireDatabaseModule
   ],
   providers: [
     ...SERVICES
